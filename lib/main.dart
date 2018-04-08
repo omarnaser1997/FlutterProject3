46070c6512c0907ec58fbab068fdff98b3c213b8
omarnaser1997 <omarnaser1997@gmail.com>
2018-04-08 10:30:11 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp( 
+    new MaterialApp(
+    home: new AwesomeButton()
+    )
+  );
+
+}
+
+
+class AwesomeButton extends StatefulWidget{
+  @override
+  AwesomeButtonState createState()=> new AwesomeButtonState();
+}
+
+class AwesomeButtonState extends State<AwesomeButton>{
+
+  int counter=0;
+  List<String> strings =["Fluter","awesome","omar"];
+
+  String displayedString="";
+
+  void onprased(){
+    setState(() {
+    displayedString=strings[counter];
+
+    if(counter<2){
+      counter++;
+
+    }else{counter=0;}
+    
+          
+        });
+    
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("welcome omar"),backgroundColor: Colors.deepOrange),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+
+           
+            new Text(displayedString,style: new TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold)),
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new RaisedButton(
+              child: new Text("click me",style: new TextStyle(color: Colors.white,fontStyle: FontStyle.italic,fontSize: 20.0,)),
+              color: Colors.red,
+              onPressed: onprased,
+            )
+
+           ] 
+          )
+        )
+      )
+    );
+  }
+
+}
